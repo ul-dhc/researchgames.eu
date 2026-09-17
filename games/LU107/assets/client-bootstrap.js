a98691924404095ce4064e1218354ae21b8baf5b
sanitareins <sanita.reinsone@gmail.com>
2026-09-17 18:41:59 +0300

    Stabilize LU-107 game loading

---
+++ games/LU107/assets/client-bootstrap.js
@@ -0,0 +1,22 @@
+import { i as reactFactory, t as reactDomFactory } from './framework-CXnKph_e.js';
+import App from './page-endash.js?v=language-hash2';
+import './wall-forward.js?v=logo-home1';
+
+const React = reactFactory();
+const ReactDOM = reactDomFactory();
+const requestedLanguage = window.location.hash === '#en' ? 'en' : 'lv';
+
+try {
+  localStorage.setItem('lu107-language', requestedLanguage);
+} catch {}
+
+document.documentElement.lang = requestedLanguage;
+document.title = requestedLanguage === 'en'
+  ? 'How Well Do You Know the University of Latvia?'
+  : 'Cik labi Tu pazīsti Latvijas Universitāti?';
+
+ReactDOM.hydrateRoot(
+  document.getElementById('root'),
+  React.createElement(App),
+  { onRecoverableError() {} },
+);
